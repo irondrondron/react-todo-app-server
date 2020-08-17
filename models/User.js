dccee2300976b78a29deec0aa1cdee617d53575c
Andrey Androsov <irondrondron@gmail.com>
2020-08-18 00:48:13 +0300

    tried to connect to db, created register method

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const db = require('../database/db');
+
+module.exports = db.sequelize.define(
+  'user',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    login: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    todo: {
+      type: Sequelize.STRING,
+    },
+    created: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
