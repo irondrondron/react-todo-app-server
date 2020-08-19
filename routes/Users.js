6ea6c3ccd79b64ae389787c2ada974dfa5677fb7
Andrey Androsov <irondrondron@gmail.com>
2020-08-20 01:48:24 +0300

    date of creation will be soon

--- routes/Users.js
+++ routes/Users.js
@@ -14,7 +14,6 @@ users.post('/register', (req, res) => {
   const userData = {
     username: req.body.username,
     password: req.body.password,
-    created: today,
   };
   User.findOne({
     where: { username: req.body.username },
@@ -60,6 +59,29 @@ users.post('/login', (req, res) => {
       res.status(400).json({
         error: err,
       });
+    });
+});
+
+users.get('/profile', (req, res) => {
+  var decoded = jwt.verify(
+    req.headers['authorization'],
+    process.env.SECRET_KEY
+  );
+
+  User.findOne({
+    where: {
+      id: decoded.id,
+    },
+  })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.send('User does not exist');
+      }
+    })
+    .catch((err) => {
+      res.send('error: ' + err);
     });
 });
 
